27361dca478e2d684361db102d0ed6ad6797e872
Andrew Streater <streater.andrew@gmail.com>
2024-04-06 14:56:08 -0700

    lists seeded

---
+++ app/seeds/lists.py
@@ -0,0 +1,39 @@
+from app.models import db, List, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_lists():
+    list1 = List(
+        name="My First List", user_id=1, private=False)
+    list2 = List(
+        name="My Second List", user_id=1, private=False)
+    list3 = List(
+        name="My Third List", user_id=3, private=True)
+    list4 = List(
+        name="Hirono", user_id=3, private=False)
+    list5 = List(
+        name="Top Picks", user_id=3, private=True)
+
+    db.session.add(list1)
+    db.session.add(list2)
+    db.session.add(list3)
+    db.session.add(list4)
+    db.session.add(list5)
+
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_lists():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.lists RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM lists"))
+
+    db.session.commit()
