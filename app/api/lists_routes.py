95d56c8932bf7ed4155392546d04fcb4a1a67e58
Andrew Streater <streater.andrew@gmail.com>
2024-04-09 20:44:18 -0700

    get lists of current user complete

--- app/api/lists_routes.py
+++ app/api/lists_routes.py
@@ -3,3 +3,25 @@ from flask_login import current_user, login_required
 from app.models import List, Item, User, Image, db
 
 lists_routes = Blueprint('lists', __name__)
+
+@lists_routes.route('/current')
+@login_required
+def get_lists_by_current_user():
+    userLists = List.query.filter(List.user_id == current_user.id)
+    response = {
+        "Lists": {
+            "byId": [],
+            "allLists": []
+        }
+    }
+    if not userLists:
+        response = jsonify({"error": "You don't have any lists yet"})
+        response.status_code = 404
+        return response
+
+    for lst in userLists:
+        list_data = lst.to_dict()
+        response['Lists']["byId"].append(list_data["listId"])
+        response['Lists']["allLists"].append(list_data)
+
+    return response
