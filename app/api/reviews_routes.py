c45d49cb458bb5ae639d584d2aaecd0d43342fab
Andrew Streater <streater.andrew@gmail.com>
2024-04-09 19:05:54 -0700

    beginning of day 4/9/24

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -26,24 +26,27 @@ def get_reviews_by_current_user():
 
     return response
 
-# @reviews_routes.route('/current')
-# @login_required
-# def get_reviews_of_current_user():
-#     reviews = Review.query.filter(Review.user_id == current_user.id)
-#     response = {'Reviews': {
-#         'byId': [],
-#         'allReviews': []
-#     }}
+@reviews_routes.route('/current')
+@login_required
+def get_reviews_of_current_user():
+    reviews = Review.query.all()
+    response = {'Reviews': {
+        'byId': [],
+        'allReviews': []
+    }}
 
-#     if not reviews:
-#             response = jsonify({"error": "You haven't left any review yet"})
-#             response.status_code = 404
-#             return response
+    if not reviews:
+            response = jsonify({"error": "You haven't left any review yet"})
+            response.status_code = 404
+            return response
 
-#     for review in reviews:
-#         review_data = review.to_dict()
-#         trade = Trade.query.get(review_data['tradeId']).to_dict()
-#         response['Reviews']['byId'].append(review_data['reviewId'])
-#         response['Reviews']['allReviews'].append(review_data)
+    for review in reviews:
+        review_data = review.to_dict()
+        buyerItem = Item.query.get(review_data['buyerItemId']).to_dict()
+        sellerItem = Item.query.get(review_data['sellerItemId']).to_dict()
+        # if sellerItem
+        trade = Trade.query.get(review_data['tradeId']).to_dict()
+        response['Reviews']['byId'].append(review_data['reviewId'])
+        response['Reviews']['allReviews'].append(review_data)
 
-#     return response
+    return response
