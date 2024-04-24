a10aa191b36f8c23e3960c2c20466f2ab3db91c0
Andrew Streater <streater.andrew@gmail.com>
2024-04-23 21:02:06 -0700

    commit

--- react-vite/src/components/ItemTileDisplay/AddToListModal.jsx
+++ react-vite/src/components/ItemTileDisplay/AddToListModal.jsx
@@ -2,13 +2,12 @@ import { fetchGetCurrentUsersLists } from "../../redux/lists"
 import { useSelector, useDispatch } from "react-redux"
 import { useModal } from "../../context/Modal";
 import { useEffect } from "react"
-// import { useNavigate } from "react-router-dom"
 import ListsDisplay from '../ListsDisplay'
 import "./AddToList.css"
 
 function AddToListModal ({ itemId }) {
     const lists = useSelector(state => state.lists.currentUserLists);
-    const sessionUser = useSelector(state => state.session.user);
+    // const sessionUser = useSelector(state => state.session.user);
     const dispatch = useDispatch()
     // const navigate = useNavigate()
 
@@ -17,9 +16,6 @@ function AddToListModal ({ itemId }) {
     }, [dispatch])
 
     const listArray = Object.values(lists)
-
-    const { closeModal } = useModal();
-
 
     // console.log("--------------LINE 18: ", listArray)
     return (
