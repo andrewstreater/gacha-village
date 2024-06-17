93c13d4bc86e74ba1e14eca9f80b01ea39117d53
Andrew Streater <streater.andrew@gmail.com>
2024-06-16 21:56:00 -0700

    trade details

--- react-vite/src/components/TradeDetails/TradeDetails.jsx
+++ react-vite/src/components/TradeDetails/TradeDetails.jsx
@@ -1,22 +1,41 @@
-// import { fetchGetTradeDetails } from "../../redux/trades"
-// import { useSelector, useDispatch } from "react-redux"
-// import { useEffect } from "react"
-// import { useParams } from "react-router-dom"
+import { fetchGetTradeDetails } from "../../redux/trades"
+import { fetchGetItems } from "../../redux/items"
+import { useSelector, useDispatch } from "react-redux"
+import { useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom"
 import "./TradeDetails.css"
 
 function TradeDetails() {
-    // const tradeDetails = useSelector(state => state.trades.tradeDetails);
-    // const dispatch = useDispatch()
-    // const navigate = useNavigate()
-    // const { tradeId } = useParams()
+    const tradeDetails = useSelector(state => state.trades.tradeDetails);
+    const sessionUser = useSelector((store) => store.session.user);
+    const allItems = useSelector(state => state.items.allItems);
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const { tradeId } = useParams()
+    let userMadeOffer
 
-    // useEffect(() => {
-    //     dispatch(fetchGetTradeDetails(tradeId))
-    // }, [dispatch, tradeId])
+    useEffect(() => {
+        dispatch(fetchGetItems())
+        dispatch(fetchGetTradeDetails(tradeId))
+    }, [dispatch, tradeId])
+
+    // if (allItems[tradeDetails.Buyer.id].ownerId == sessionUser.id){
+    //     userMadeOffer = true
+    // }
+    // if (allItems[tradeDetails.Seller.id].ownerId == sessionUser.id) {
+    //     userMadeOffer = false
+    // }
+
+    console.log('TRADE DETAILS userMadeOffer: ', userMadeOffer)
 
     return (
         <>
-        <h1>Trade Details page</h1>
+        <h1>Trade Details</h1>
+        <div>{tradeDetails.Buyer.username}</div>
+        <div>{tradeDetails.Buyer.username}</div>
+        <div>{tradeDetails.Buyer.Item.title}</div>
+        <div>{tradeDetails.Seller.username}</div>
+        <div>{tradeDetails.Seller.Item.title}</div>
         </>
     )
 }
--- react-vite/src/components/TradeList/TradeListTile.jsx
+++ react-vite/src/components/TradeList/TradeListTile.jsx
@@ -1,17 +1,23 @@
-import { fetchGetItems } from "../../redux/items"
-import { useSelector, useDispatch } from "react-redux"
-import { useEffect } from "react";
+// import { fetchGetItems } from "../../redux/items"
+// import { useSelector, useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+// import { useEffect } from "react";
 import "./TradeListTile.css"
 
 function TradeListTile ({trade, allItems, currentUserId}) {
 
-    console.log('From TradeListTile:', trade)
+    const navigate = useNavigate()
+
+    // console.log('From TradeListTile:', trade)
 
     if (allItems[trade.buyerItemId].ownerId == currentUserId) {
         return (
-            <div className="trade-list-tile">
+            <div className="trade-list-tile" onClick={() => navigate(`/trades/${trade.tradeId}`)}>
                 <div> Trade# {trade.tradeId}</div>
-                <div className={trade.status}>{trade.status}</div>
+                <div className="status-tile">
+                    <div className="status-dot"></div>
+                    <div className={trade.status}>{trade.status}</div>
+                </div>
                 <div className="row-two trade-tile-text" >You offered: </div>
                 <img className="item-image-icon row-two" src={allItems[trade.buyerItemId].previewImage.imageUrl}></img>
                 <div className="row-two trade-tile-text">for</div>
@@ -21,9 +27,17 @@ function TradeListTile ({trade, allItems, currentUserId}) {
     }
     if (allItems[trade.sellerItemId].ownerId == currentUserId) {
         return (
-            <>
-            <h2>You got an offer on trade {trade.tradeId}</h2>
-            </>
+            <div className="trade-list-tile" onClick={() => navigate(`/trades/${trade.tradeId}`)}>
+                <div> Trade# {trade.tradeId}</div>
+                <div className="status-tile">
+                    <div className="status-dot"></div>
+                    <div className={trade.status}>{trade.status}</div>
+                </div>
+                <div className="row-two trade-tile-text" >You were offered: </div>
+                <img className="item-image-icon row-two" src={allItems[trade.buyerItemId].previewImage.imageUrl}></img>
+                <div className="row-two trade-tile-text">for</div>
+                <img className="item-image-icon row-two" src={allItems[trade.sellerItemId].previewImage.imageUrl}></img>
+            </div>
         )
     }
 
